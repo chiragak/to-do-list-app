1bca1994bcaaa2fb0130d8394395084a5b81aef1
Chirag A K <chiragajekar@gmail.com>
2024-11-07 04:35:03 +0530

    added not-found page and other minor changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,20 @@
-import './globals.css';  
+"use client";
+import './globals.css';
+import { ThemeProvider } from 'next-themes';
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>To-Do List App</title>
-        
       </head>
-      <body className="bg-[#F5F9FF] font-sans">
-        <main>{children}</main> 
-      </body>
+      <ThemeProvider attribute="data-theme" defaultTheme="light" enableSystem>
+        <body>
+          <main>{children}</main>
+        </body>
+      </ThemeProvider>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 module.exports = {
+  darkMode: 'class', // This enables dark mode based on the class 'dark'
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx}',  
-    './src/components/**/*.{js,ts,jsx,tsx}',  
+    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Rubik', 'sans-serif'], // Rubik font family
+        sans: ['Rubik', 'sans-serif'],
       },
       colors: {
-        'primary-dark': '#11175E', // Custom color for primary dark text
+        'primary-dark': '#11175E',
       },
     },
   },
