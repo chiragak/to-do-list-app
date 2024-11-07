19d506e0491fdd548fec256fa7d234221ee13663
Chirag A K <chiragajekar@gmail.com>
2024-11-07 10:36:15 +0530

    added responsive design

--- src/app/page.js
+++ src/app/page.js
@@ -24,21 +24,19 @@ export default function Home() {
   };
 
   return (
-    
-    <div className="flex flex-col h-screen items-center justify-center">
-       <ThemeToggle />
+    <div className="flex flex-col min-h-screen items-center justify-center p-4 md:p-6">
+      <ThemeToggle />
       <div 
-        className="bg-white px-48 py-12 w-[1000px] h-[670px] rounded-[32px] relative"
+        className="bg-white w-full max-w-[1000px] min-h-[480px] md:min-h-[670px] rounded-lg md:rounded-[32px] relative px-4 py-8 md:px-48 md:py-12"
         style={{ boxShadow: '0px 0px 4px 0px #00174726' }}
       >
-
-        <h1 className="text-5xl font-semibold mb-10 font-rubik text-primary-dark my-10">
+        <h1 className="text-3xl md:text-5xl font-semibold mb-6 md:mb-10 font-rubik text-primary-dark md:my-10">
           Daily To Do List
         </h1>
 
         <AddTaskForm onAddTask={addTask} />
 
-        <div className="mt-4 space-y-4">
+        <div className="mt-4 space-y-4 mb-24">
           {tasks.map((task) => (
             <Task key={task.id} task={task} onMarkCompleted={markCompleted} />
           ))}
@@ -46,12 +44,12 @@ export default function Home() {
 
         {/* Horizontal Line */}
         <div 
-          className="absolute bottom-20 left-48 right-48"
+          className="absolute bottom-20 left-4 right-4 md:left-48 md:right-48"
           style={{ borderTop: '1px solid #B1BACB' }}
         />
 
         {/* Footer with Total Items and Clear All */}
-        <div className="absolute bottom-8 left-48 right-48 flex justify-between items-center">
+        <div className="absolute bottom-8 left-4 right-4 md:left-48 md:right-48 flex justify-between items-center">
           <span style={{ color: '#B1BACB' }}>
             {tasks.length} items
           </span>
